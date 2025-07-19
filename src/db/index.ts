308d86ae6b0842ab5d95e5e1d4a11c9326e5a30d
Phan Long <longbp166@gmail.com>
2025-07-20 00:10:46 +0700

    refactor(db): update mongoose connection auth syntax

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,9 +5,7 @@ export async function initDbConnection() {
 	const url = `mongodb://${Env.DB_HOST}:${Env.DB_PORT}`;
 	await mongoose.connect(url, {
 		dbName: Env.DB_NAME,
-		auth: {
-			username: Env.DB_USER,
-			password: Env.DB_PASS,
-		},
+		user: Env.DB_USER,
+		pass: Env.DB_PASS,
 	});
 }
